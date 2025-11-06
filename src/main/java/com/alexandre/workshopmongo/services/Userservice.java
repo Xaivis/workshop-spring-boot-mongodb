202b641d673840a2ee9d6a050972ee0df089963f
Xaivis <ivis59@gmail.com>
2025-11-06 20:22:59 -0300

    Atualização de usuário com PUT

--- src/main/java/com/alexandre/workshopmongo/services/Userservice.java
+++ src/main/java/com/alexandre/workshopmongo/services/Userservice.java
@@ -16,27 +16,38 @@ public class Userservice {
 
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.orElseThrow(
-			() -> new ObjectNotFoundException("Objeto não encontrado"));
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
+
 	public User insert(User obj) {
 		return repository.insert(obj);
 	}
-	
+
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
-	
+
 	public void delete(String id) {
 		findById(id);
 		repository.deleteById(id);
+	}
+
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repository.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+
 	}
 }
